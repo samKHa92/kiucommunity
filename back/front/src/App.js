c96500d94a5710071e51996d79f8565c9647b21e
samKHa92 <samkha@gfmstudio.com>
2021-09-09 12:37:28 +0400

    double check

--- back/front/src/App.js
+++ back/front/src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Click me!</p>
         <a
           className="App-link"
           href="https://kiu.edu.ge"
